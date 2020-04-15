e81eb0895f7ec4f771cba0030ce76b26e816ff7a
adiathasan <61478071+adiathasan@users.noreply.github.com>
2020-04-14 23:56:27 -0700

    Update settings.py

--- profit/settings.py
+++ profit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h^)!==r)segu@#84nv-m!^i$ygps_pfntfijfdjj)!5(yxhjo#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['adiats-bloging-app.herokuapp.com', '127.0.0.1']
 
